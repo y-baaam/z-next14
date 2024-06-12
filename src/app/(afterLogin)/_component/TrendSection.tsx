7c803b5837c345e7c48ccfa8e16808f546646095
young beom <dudqja3674@gmail.com>
2024-06-12 15:09:06 +0900

    feat: trend section

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+"use client";
+
+import { usePathname } from "next/navigation";
+import styles from "./trendSection.module.css";
+import Trend from "./Trend";
 
 export default function TrendSection() {
-  return <div></div>;
+  const pathname = usePathname();
+  if (pathname === "/explore") return null;
+
+  return (
+    <div className={styles.trendBg}>
+      <div className={styles.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </div>
+  );
 }
