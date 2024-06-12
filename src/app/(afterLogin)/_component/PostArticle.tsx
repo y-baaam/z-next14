ea2233668ef823a4ccba784bb9317108dfb8e77e
young beom <dudqja3674@gmail.com>
2024-06-12 22:23:15 +0900

    feat: post

---
+++ src/app/(afterLogin)/_component/PostArticle.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ReactNode } from "react";
+import styles from "./post.module.css";
+
+type Props = {
+  children: ReactNode;
+  post: {
+    postId: number;
+    content: string;
+    User: {
+      id: string;
+      nickname: string;
+      image: string;
+    };
+    createdAt: Date;
+    Images: any[];
+  };
+};
+
+export default function PostArticle({ children, post }: Props) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/${post.User.id}/status/${post.postId}`);
+  };
+
+  return (
+    <article onClickCapture={onClick} className={styles.post}>
+      {children}
+    </article>
+  );
+}
